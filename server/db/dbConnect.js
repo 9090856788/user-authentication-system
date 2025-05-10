44529b20761a48ad484c0624c11cd6fd305a2403
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2025-05-10 20:09:34 +0530

    write the logic for mongodb database connection

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
-const dbConnect = () => {
-
-}
+const dbConnect = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => {
+        console.log(`Database Connected Successfully :)`);
+      })
+      .catch(() => {
+        console.log(`Database Connection was failed due to : ${error.message}`);
+      });
+  } catch (error) {
+    console.error(`Database connection error: ${error.message}`);
+  }
+};
 
 export default dbConnect;
--- server/server.js
+++ server/server.js
@@ -12,20 +12,18 @@ const app = express();
 //default middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "url",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // api endpoints
 
 // db connection
 dbConnect();
 
-
-app.get("/", (req,res) => {
-    res.send("Api working")
-})
 app.listen(PORT, () => {
-    console.log(`Server running on the PORT ${PORT} ):`)
-})
+  console.log(`Server running on the PORT ${PORT} :)`);
+});
