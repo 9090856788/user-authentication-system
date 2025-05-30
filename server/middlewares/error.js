81b4ffd1472c9c84bd8be241981df3addf9a1c8e
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2025-05-30 11:18:14 +0530

    created custom error handler

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,1 +1,61 @@
-// error
+class ErrorHandler extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export const errorMiddleware = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server error";
+
+  if (err.name === "CastError") {
+    return res.status(400).json({
+      success: false,
+      message: `Resource not found. Invalid: ${err.path}`,
+    });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  } else if (err.name === "JsonWebTokenError") {
+    return res.status(401).json({
+      success: false,
+      message: "Json Web Token is invalid, try again",
+    });
+  } else if (err.name === "TokenExpiredError") {
+    return res.status(401).json({
+      success: false,
+      message: "Json Web Token is expired, try again",
+    });
+  } else if (err.name === "MongoServerError" && err.code === 11000) {
+    return res.status(400).json({
+      success: false,
+      message: `Duplicate ${Object.keys(err.keyValue)} entered`,
+    });
+  } else if (err.name === "MulterError") {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  } else if (err.name === "Error") {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  } else if (err.name === "TypeError") {
+    return res.status(400).json({
+      success: false,
+      message: "Type Error occurred",
+    });
+  } else {
+    return res.status(statusCode).json({
+      success: false,
+      message: message,
+    });
+  }
+};
+export const createError = (message, statusCode) => {
+  return new ErrorHandler(message, statusCode);
+};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import dbConnect from "./db/dbConnect.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -15,7 +16,7 @@ app.use(express.urlencoded({ extends: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "url",
+    origin: "frontend_url",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -25,8 +26,12 @@ app.use(
 app.get("/api", (req, res) => {
   res.send("API is running");
 });
+
 // db connection
 dbConnect();
+
+// error middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server running on the PORT ${PORT} :)`);
